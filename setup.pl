c94329ac48535378f0bdcedfc7f57f10e32fd0b4
Logan Gilmour <logangilmour@gmail.com>
2013-10-24 19:00:08 -0600

    Add deletion step

--- setup.pl
+++ setup.pl
@@ -5,8 +5,11 @@ foreach my $ip (@ARGV){
     my $pid=fork();
     die "Cannot fork: $!" if (! defined $pid);
     if (! $pid) {
+	print $_;
 	`ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu\@10.0.0.$ip "sudo apt-get update; sudo apt-get -y install openjdk-6-jre-headless"`;
+	print $_;
 	`tar zcf - hadoop .ssh | ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu\@10.0.0.$ip "tar zxf -"`;
+	`ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu\@10.0.0.$ip "rm -rf name data tmp mapred"`;
 	exit;
     }
 }
