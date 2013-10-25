34ed6ffe45312768362abc0c78e205cfdc7a7187
Logan Gilmour <logangilmour@gmail.com>
2013-10-24 18:40:25 -0600

    Fix perl

--- setup.pl
+++ setup.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 foreach my $ip (@ARGV){
-    $pid=fork();
+    my $pid=fork();
     die "Cannot fork: $!" if (! defined $pid);
     if (! $pid) {
 	`ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ubuntu\@10.0.0.$ip "sudo apt-get update; sudo apt-get -y install openjdk-6-jre-headless"`;
@@ -10,3 +10,5 @@ foreach my $ip (@ARGV){
 	exit;
     }
 }
+
+
